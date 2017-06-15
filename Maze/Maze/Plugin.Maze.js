42ad51d53b6ea296c8fd50a8b8f90903708f50fa
ifat neumann <ineumann19@gmail.com>
2017-06-15 16:57:11 +0300

    bug with pictures and maze

--- Maze/Maze/Plugin.Maze.js
+++ Maze/Maze/Plugin.Maze.js
@@ -23,19 +23,23 @@
 (function ($) {
     $.fn.drawMaze = function(mazeData, // the matrix containing the maze cells
         rows,
-        cols
-        //startRow,
-        //startCol, // initial position of the player
-        //exitRow,
-        //exitCol, // the exit position
-        //playerImage, // player's icon (of type Image)
-        //exitImage, // exit's icon (of type Image)
+        cols,
+        startRow,
+        startCol, // initial position of the player
+        exitRow,
+        exitCol, // the exit position
+        playerImage, // player's icon (of type Image)
+        exitImage // exit's icon (of type Image)
        // true, // is the board enabled (i.e., player can move)
         ){
         var mazeCanvas = this.get(0);
         var context = mazeCanvas.getContext("2d");
         var cellWidth = mazeCanvas.width / cols;
         var cellHeight = mazeCanvas.height / rows;
+        var start = new Image();
+        start.src = playerImage;
+        var end = new Image();
+        end.src = exitImage;
         for (var i = 0; i < rows; i++) {
             for (var j = 0; j < cols; j++) {
                 if (mazeData[i*rows+j] == 1) {
@@ -43,6 +47,8 @@
                 }
             }
         }
+        context.drawImage(start.src, cellWidth * startCol, cellHeight * startRow, cellWidth, cellHeight);
+        context.drawImage(end.src, cellWidth * startCol, cellHeight * startRow, cellWidth, cellHeight);
         return this;
     };
 })(jQuery);
