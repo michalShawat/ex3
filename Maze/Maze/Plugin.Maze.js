eb31371c0f61d8e2cdc6a640b3fbed60460b6f31
ifat neumann <ineumann19@gmail.com>
2017-06-15 15:46:16 +0300

    maze works!!!

---
+++ Maze/Maze/Plugin.Maze.js
@@ -0,0 +1,48 @@
+﻿//(function ($) {
+//    $.fn.drawMaze = function (maze) {
+//    var mazeCanvas = this.get(0);
+//    var context = mazeCanvas.getContext("2d");
+//    var realMaze = maze.toJSON();
+//    var hi = realMaze[0];
+//    var rows = maze.rows;
+//    var cols = maze.cols;
+//    var cellWidth = mazeCanvas.width / cols;
+//    var cellHeight = mazeCanvas.height / rows;
+//    for (var i = 0; i < rows; i++) {
+//         for (var j = 0; j < cols; j++) {
+//              if (maze[i][j] == 1) {
+//                   context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+//             }
+
+//         }
+//    }
+//    return this;};
+//})(jQuery);
+
+
+(function ($) {
+    $.fn.drawMaze = function(mazeData, // the matrix containing the maze cells
+        rows,
+        cols
+        //startRow,
+        //startCol, // initial position of the player
+        //exitRow,
+        //exitCol, // the exit position
+        //playerImage, // player's icon (of type Image)
+        //exitImage, // exit's icon (of type Image)
+       // true, // is the board enabled (i.e., player can move)
+        ){
+        var mazeCanvas = this.get(0);
+        var context = mazeCanvas.getContext("2d");
+        var cellWidth = mazeCanvas.width / cols;
+        var cellHeight = mazeCanvas.height / rows;
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (mazeData[i*rows+j] == 1) {
+                    context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                }
+            }
+        }
+        return this;
+    };
+})(jQuery);
