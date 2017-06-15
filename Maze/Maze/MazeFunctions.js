91f1c2a0b5b56f050e34093fe62ca9d20e7f754c
ifat neumann <ineumann19@gmail.com>
2017-06-15 21:36:01 +0300

    ghi

---
+++ Maze/Maze/MazeFunctions.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $("#startLink").click(function (event) {
+        var name = $("#mazeName").val();
+        var rows = $("#mazeRows").val();
+        var cols = $("#mazeCols").val();
+        $.getJSON("api/generate/" + name + "/" + rows + "/" + cols,
+            function (data) {
+                //var mazemaze = $("#mazeCanvas").drawMaze(data);
+                var mazeData = data.Maze; // the matrix containing the maze cells
+                var startRow = data.Start.Row;
+                var startCol = data.Start.Col; // initial position of the player
+                var exitRow = data.End.Row;
+                var exitCol = data.End.Col; // the exit position
+                var playerImage = document.getElementById("marco"); // player's icon (of type Image)
+                var exitImage = document.getElementById("mother");
+                $("#mazeCanvas").drawMaze(
+                    mazeData, // the matrix containing the maze cells
+                    rows,
+                    cols,
+                    startRow,
+                    startCol, // initial position of the player
+                    exitRow,
+                    exitCol, // the exit position
+                    playerImage, // player's icon (of type Image)
+                    exitImage // exit's icon (of type Image)
+                    //true, // is the board enabled (i.e., player can move)
+                    //function(direction, playerRow, playerCol) {
+                    //    // a callback function which is invoked after each move
+                );
+
+                document.title = name;
+            });
+    });
+});
