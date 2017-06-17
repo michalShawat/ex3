43a2743ed223585a6c6076e54c93566b186dcc6a
ifat neumann <ineumann19@gmail.com>
2017-06-18 00:56:23 +0300

    settings done!

--- Maze/Maze/Settings.js
+++ Maze/Maze/Settings.js
@@ -1,6 +1,8 @@
-﻿    localStorage.setItem("rows", "6");
-    localStorage.setItem("cols", "6");
+﻿$(document).ready(function() {
+    localStorage.setItem("rows", "7");
+    localStorage.setItem("cols", "7");
     localStorage.setItem("algoType", 0);
+    document.getElementById("mazeRows").value = localStorage.getItem("rows");
     document.getElementById("mazeRows").value = localStorage.getItem("rows");
     $("#saveLink").click(function(event) {
         if (localStorage.getItem("rows").localeCompare($("#mazeRows").val())) {
@@ -14,12 +16,5 @@
         if (document.getElementById("algoSelect").selectedIndex == 1) {
             localStorage.setItem("algoType", 1);
         }
-
-        alert("rows: " + localStorage.getItem("rows"));
-        alert("cols: " + localStorage.getItem("cols"));
-        alert("algo: " + localStorage.getItem("algoType"));
     });
-
-    $(document).before(function() {
-        document.getElementById("mazeRows").value = localStorage.getItem("rows");
-    });
+});
