5dedf258e6d0334fabbca94683bbaf7646cb5e4d
ifat neumann <ineumann19@gmail.com>
2017-06-18 00:11:41 +0300

    settings

--- Maze/Maze/Settings.js
+++ Maze/Maze/Settings.js
@@ -1,24 +1,25 @@
-﻿$(document).ready(function() {
-    localStorage.setItem("rows", "6");
+﻿    localStorage.setItem("rows", "6");
     localStorage.setItem("cols", "6");
     localStorage.setItem("algoType", 0);
-    
-    $("#saveLink").click(function (event) {
-        if ((localStorage.getItem("rows").localeCompare($("#mazeRows").val()))) {
-            localStorage.clear("rows");
+    document.getElementById("mazeRows").value = localStorage.getItem("rows");
+    $("#saveLink").click(function(event) {
+        if (localStorage.getItem("rows").localeCompare($("#mazeRows").val())) {
             localStorage.setItem("rows", $("#mazeRows").val());
         }
 
-        if ((localStorage.getItem("cols").localeCompare($("#mazeRows").val()))) {
-            localStorage.setItem("cols", document.getElementById("mazeCols"));
+        if (localStorage.getItem("cols").localeCompare($("#mazeCols").val())) {
+            localStorage.setItem("cols", $("#mazeCols").val());
         }
 
         if (document.getElementById("algoSelect").selectedIndex == 1) {
-            localStorage.setItem("rows", 1);
+            localStorage.setItem("algoType", 1);
         }
-        
-        alert("rows: "+localStorage.getItem("rows"));
-        alert("cols: "+localStorage.getItem("cols"));
-        alert("algo: "+localStorage.getItem("algoType"));
+
+        alert("rows: " + localStorage.getItem("rows"));
+        alert("cols: " + localStorage.getItem("cols"));
+        alert("algo: " + localStorage.getItem("algoType"));
     });
-});
+
+    $(document).before(function() {
+        document.getElementById("mazeRows").value = localStorage.getItem("rows");
+    });
