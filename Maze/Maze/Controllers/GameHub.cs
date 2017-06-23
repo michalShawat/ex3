dc3429a873b1f8678a18201e31d713f3424fb7b1
m1245 <m1245@DESKTOP-VVLE821>
2017-06-23 10:27:16 +0300

    start multi

---
+++ Maze/Maze/Controllers/GameHub.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using Microsoft.AspNet.SignalR;
+
+namespace Maze.Controllers
+{
+    public class GameHub : Hub
+    {
+        private static ConcurrentDictionary<string, string> connectedUsers =
+           new ConcurrentDictionary<string, string>();
+
+        public void Connect(string name)
+        {
+            connectedUsers[name] = Context.ConnectionId;
+        }
+
+        public void SendMessage(string senderName, string recipientName, string text)
+        {
+            string recipientId = connectedUsers[recipientName];
+            if (recipientId == null)
+                return;
+            Clients.Client(recipientId).gotMessage(senderName, text);
+        }
+    }
+}
