9d7171939421a3c20f72d5e80d7c71fa951bed68
ifat neumann <ineumann19@gmail.com>
2017-06-25 12:33:57 +0300

    hihihihi

---
+++ Maze/Maze/userRankings.js
@@ -0,0 +1,14 @@
+﻿window.onload = function getAllUsers() {
+    $.getJSON("../api/Users").done(function(usersList) {
+        //usersList = sortBykey(usersList, "Rank");
+
+        var tr;
+        for (var i = 0; i < usersList.length; ++i) {
+            tr = $("<tr/>");
+            tr.append("<th/>" + usersList[i].Rank + "<th/>");
+            tr.append("<th/>" + usersList[i].UserName + "<th/>");
+            tr.append("<th/>" + usersList[i].Wins + "<th/>");
+            tr.append("<th/>" + usersList[i].Losses + "<th/>");
+        }
+    })
+}
