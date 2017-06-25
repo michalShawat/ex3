c932808bd548c1804516c77f0b63886a3f2d932b
m1245 <m1245@DESKTOP-VVLE821>
2017-06-25 18:01:59 +0300

    defult

--- Maze/Maze/Settings.js
+++ Maze/Maze/Settings.js
@@ -3,14 +3,14 @@
     localStorage.setItem("cols", "7");
     localStorage.setItem("algoType", 0);
     document.getElementById("mazeRows").value = localStorage.getItem("rows");
-    document.getElementById("mazeRows").value = localStorage.getItem("rows");
+    document.getElementById("mazeCols").value = localStorage.getItem("cols");
     $("#saveLink").click(function(event) {
         if (localStorage.getItem("rows").localeCompare($("#mazeRows").val())) {
             localStorage.setItem("rows", $("#mazeRows").val());
         }
 
         if (localStorage.getItem("cols").localeCompare($("#mazeCols").val())) {
-            localStorage.setItem("cols", $("#mazeCols").val());
+             localStorage.setItem("cols", $("#mazeCols").val());
         }
 
         if (document.getElementById("algoSelect").selectedIndex == 1) {
